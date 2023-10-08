be222d6b5d17bac2052faa86acb4c2b5883710dc
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-10-07 22:45:57 -0300

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace tasksUI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            Label.Text = e.NewTextValue;
+        }
+    }
+}
