86e20af02b66c6325a1fd10c987500b5a0afd9f1
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2023-10-14 18:54:54 -0300

    adding tasks to a list and displaying it on screen, is still ugly

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -4,9 +4,42 @@ namespace tasksUI
 {
     public partial class MainPage : ContentPage
     {
+        List<string> tasks = new();
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void HandleAddTask(object sender, EventArgs e)
+        {
+            tasks.Add(TaskTitleInput.Text);
+            UpdateTasksStack();
+        }
+
+        void UpdateTasksStack()
+        {
+            TasksStackLayout.Children.Clear();
+
+            foreach (var task in tasks)
+            {
+                Label tModel = TaskModel(task);
+                TasksStackLayout.Add(tModel);
+            }
+
+        }
+
+        Label TaskModel(string taskTitle) 
+        {
+            Label taskModel = new Label()
+            {
+                Text = taskTitle,
+                Margin = 15,
+                BackgroundColor = new Color(255, 255, 255),
+                TextColor= new Color(0, 0, 0)
+            };
+
+            return taskModel;
         }
     }
 }
